15600084f8ec5d94a06ec82d57eb3a7ed412fd69
Pavel <ahp39490@gmail.com>
2021-09-18 13:54:17 -0400

    First commit to the git practice project

---
+++ DataTypes.js
@@ -0,0 +1,46 @@
+let fName = "Pavel"
+console.log(typeof fName)
+console.log("First naem is "+fName)
+
+let marks = 99;
+console.log(typeof marks);
+console.log(marks)
+
+let flag = false
+console.log(typeof flag);
+console.log(flag);
+
+let address = null
+console.log(typeof address);
+console.log(address);
+
+
+let x = 10;
+console.log(typeof x);
+console.log(x);
+
+x = 10.1
+console.log(typeof x);
+console.log(x);
+
+x = true
+console.log(typeof x);
+console.log(x);
+
+x = undefined
+console.log(typeof x);
+console.log(x);
+
+x = null
+console.log(typeof x);
+console.log(x);
+
+let person = {
+    "fName" : "Pavel",
+    "lName" : "Ahmed"
+}
+
+// let person = {}
+console.log(person);
+console.log(typeof person);
+
